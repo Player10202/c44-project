235587b2a237a0461b93d6f8f7861b85f2c70f68
Player10202 <114907799+Player10202@users.noreply.github.com>
2023-05-08 20:07:17 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+var kong,kongImg,kongjumpImg
+var helicopter, lefthelicopterImg, righthelicopterImg
+var backgroundimg
+var invisiground
+var invisiground2, invisiground3
+var helicopter1,helicopter2,helicopter3,helicopter4, helicopter5,helicopter6
+
+function preload(){
+backgroundimg=loadImage("background.jpg")
+kongImg=loadImage("kingkongstand.png")
+kongjumpImg=loadImage("kingkongjump.png")
+lefthelicopterImg=loadImage("helileft.png")
+righthelicopterImg=loadImage("heliright.png")
+
+}
+
+function setup() {
+  
+
+  
+
+
+
+}
+
+function draw() {
+  background(backgroundimg);  
+
+  
+
+  drawSprites();
+}
+
+
